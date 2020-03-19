bf3f5ff8cd3492e40d7c701d58ab64ef8612e8c3
Adi-Levy <adilevy15@gmail.com>
2020-03-19 10:24:03 +0200

    changed path to polynomial in controller

--- pure_pursuit/controller.py
+++ pure_pursuit/controller.py
@@ -1,5 +1,7 @@
 import math
 import numpy as np
+from sympy import solve
+from sympy.abc import x
 
 class PurePursuitController:
     """[this a class to define a controller object of a pure pursuite controller
@@ -19,7 +21,7 @@ class PurePursuitController:
         self._kdd = kdd
         self._max_steering_angle = max_steering_angle*2*math.pi/360
         self._poly_fit_deg  = poly_fit_deg
-        self.path = []
+        self.path = np.poly1d([0,0,0,0])
         self.coordinates = []
         self.orientation = 0
         self.velocity = 0
@@ -29,67 +31,50 @@ class PurePursuitController:
         self.velocity = v
         self.orientation = orientation
 
-    def update_path(self, path): # update the path for the controller for calculations
-        self.path = path
+    def update_path(self, path, v): # update the path for the controller for calculations
+        self.path = np.poly1d(path)
+        self.update_state(0,0,v,0)
 
     def calculate_steering(self): # calc the needed steering angle to course correct to the next waypoint
-        look_ahead_point = self._calculate_look_ahead_point()
+        ld = self.velocity * self._kdd
+        look_ahead_point = self._calculate_look_ahead_point(ld)
         # print(look_ahead_point)
 
         alpha = math.atan2(look_ahead_point[1] - self.coordinates[1], look_ahead_point[0] - self.coordinates[0]) - self.orientation # error angle
-        delta = 1/math.atan2(2*self._car_length*math.sin(alpha), self._point_distance(look_ahead_point))
+        delta = 1/math.atan2(2*self._car_length*math.sin(alpha), ld)
         return (max(delta, -self._max_steering_angle) if (delta < 0) else min(delta, self._max_steering_angle))/(2*math.pi/360)
 
-    def _calculate_look_ahead_point(self):
-        ld = self.velocity * self._kdd
-        
-        near_point_index = self._find_near_point_index()
-        dist_bw_points = math.sqrt(((self.path[near_point_index][0] - self.path[near_point_index+1][0])**2) + ((self.path[near_point_index][1] - self.path[near_point_index+1][1])**2))
-        if(self._point_distance(self.path[near_point_index+1]) < dist_bw_points):
-            near_point_index += 1
+    def _calculate_look_ahead_point(self, ld):
+        x_vec = solve((self.path[3]*x**3 + self.path[2]*x**2 + self.path[1]*x + self.path[0] - self.coordinates[1])**2 + (x - self.coordinates[0])**2 - ld**2, x)
+        x_options = []
+        for x in x_vec:
+            if not np.iscomplex(x):
+                if x > self.coordinates[0]:
+                    x_options.append(x)
+    
+        return [x_options[0], self.path.polyval(x_options[0])]
 
-        f = open("stateNew.log","a+")
-        f.write( str(self.path[near_point_index])+"  |   " +str(self._point_distance(self.path[near_point_index]))+ "  |"+str(self.coordinates) + " |")
-        f.close     
+    # def _find_near_point_index(self):
+    #     near_point = self.path[0]
+    #     near_point_distance = self._point_distance(near_point)
+    #     for way_point in self.path:
+    #         way_point_distance = self._point_distance(way_point)
+    #         if (near_point_distance > way_point_distance):
+    #             near_point = way_point
+    #             near_point_distance = self._point_distance(near_point)
 
-        swapped_axis_path = np.swapaxes(self.path[max(near_point_index-4, 0):min(near_point_index+4, len(self.path)-1)], 0 , 1)
-        p = np.poly1d(np.polyfit(swapped_axis_path[0], swapped_axis_path[1], self._poly_fit_deg))
-        # print("swapped_axis_path: " + str(swapped_axis_path) + "\n" + str(self.path))
-        point_index = self._target_point_index(swapped_axis_path[0][near_point_index:], swapped_axis_path[1][near_point_index:], ld)
-        
-        x_sector = np.linspace(self.path[near_point_index+point_index-1][0], self.path[near_point_index+point_index][0], 5)
-        y_sector = np.polyval(p, x_sector)
-
-        point_index = self._target_point_index(x_sector, y_sector, ld)
-        # t1 = x_sector[point_index]
-        # t2 = y_sector[point_index]
-        f = open("stateNew.log","a+")
-        f.write( str([x_sector[point_index], y_sector[point_index]]) + ' | ' + str(ld) + ' | ' + str(x_sector) + ' | ' + str(self.path[near_point_index]) + '\n')
-        f.close     
-
-        return [x_sector[point_index], y_sector[point_index]]
-
-    def _find_near_point_index(self):
-        near_point = self.path[0]
-        near_point_distance = self._point_distance(near_point)
-        for way_point in self.path:
-            way_point_distance = self._point_distance(way_point)
-            if (near_point_distance > way_point_distance):
-                near_point = way_point
-                near_point_distance = self._point_distance(near_point)
-
-        return self.path.index(near_point)
+    #     return self.path.index(near_point)
 
     def _point_distance(self, point):
         return math.sqrt((self.coordinates[0] - point[0])**2 + (self.coordinates[1] - point[1])**2)
 
-    def _target_point_index(self, x_vector, y_vector, distance):
-        for point_index in range(len(x_vector)):
-            # print("limited path: " + str([x_vector[point_index], y_vector[point_index]]))
-            if(self._point_distance([x_vector[point_index], y_vector[point_index]]) > distance):
-                # alpha = math.atan2(y_vector[point_index] - self.coordinates[1], x_vector[point_index] - self.coordinates[0]) - self.orientation
-                # if(abs(alpha)<((math.pi)/2)):   # making sure the chosen look ahead point is not behind the car
-                    return point_index
-        return (len(x_vector)-1)
+    # def _target_point_index(self, x_vector, y_vector, distance):
+    #     for point_index in range(len(x_vector)):
+    #         # print("limited path: " + str([x_vector[point_index], y_vector[point_index]]))
+    #         if(self._point_distance([x_vector[point_index], y_vector[point_index]]) > distance):
+    #             # alpha = math.atan2(y_vector[point_index] - self.coordinates[1], x_vector[point_index] - self.coordinates[0]) - self.orientation
+    #             # if(abs(alpha)<((math.pi)/2)):   # making sure the chosen look ahead point is not behind the car
+    #                 return point_index
+    #     return (len(x_vector)-1)
 
 
