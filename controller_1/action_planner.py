64cf91aae7a2c8c80204666e5e8ff078757f2b49
orrlani <olaniado@campus.technion.ac.il>
2020-03-24 09:24:59 +0200

    orr add function _clac_ending_speed

--- controller_1/action_planner.py
+++ controller_1/action_planner.py
@@ -2,8 +2,13 @@ import numpy as np
 
 from mediator import State
 
+from math import sqrt
 A_MAX = 0.9
 DELTA_T = 0.2
+EPSILON= 10^-3
+FIRSTDITANCE = 100
+FIRSTTIME="global"
+FIRSTTIME=True
 
 
 class ActionPlanner:
@@ -11,6 +16,7 @@ class ActionPlanner:
         self.new_wheel_angle = None
         self.new_speed = None
         self.state = state
+        self.is_first_time = True
 
     def update_action(self, state: State, p: np.ndarray):
         self.state = state
@@ -24,11 +30,45 @@ class ActionPlanner:
         if self.state.dist_to_end < 0:
             self.new_speed = self._speed_upper_bound(p)
         else:
+            # calculate R
             self.new_speed = self._clac_ending_speed(self.state.dist_to_end, self.state.speed)
 
-    def _calc_ending_speed(self, dist: float, speed: float):
-        pass #TODO: add or's code
+    def _clac_ending_speed(self, dist: float, speed: float):
 
+        global FIRSTDITANCE
+        global FIRSTTIME
+        if FIRSTTIME:
+            FIRSTTIME = True
+            FIRSTDITANCE = dist
+
+        if (dist - speed * DELTA_T < EPSILON):
+            return 0
+
+        elif FIRSTDITANCE >= dist - speed * DELTA_T > FIRSTDITANCE * 1 / 2:
+            return speed
+
+        elif FIRSTDITANCE * 1 / 2 >= dist - 0.95 * speed * DELTA_T > FIRSTDITANCE * 1 / 4:
+            return speed * 0.95
+
+        elif FIRSTDITANCE * 1 / 4 >= dist - 0.925 * speed * DELTA_T > FIRSTDITANCE * 1 / 8:
+            return speed * 0.925
+
+        elif FIRSTDITANCE * 1 / 8 >= dist - 0.9 * speed * DELTA_T > FIRSTDITANCE * 1 / 16:
+            return speed * 0.9
+
+        elif FIRSTDITANCE * 1 / 16 >= dist - 0.875 * speed * DELTA_T > FIRSTDITANCE * 1 / 32:
+            return speed * 0.875
+
+        elif FIRSTDITANCE * 1 / 32 >= dist - 0.85 * speed * DELTA_T > FIRSTDITANCE * 1 / 64:
+            return speed * 0.85
+
+        elif FIRSTDITANCE * 1 / 64 >= dist - 0.825 * speed * DELTA_T > FIRSTDITANCE * 1 / 128:
+            return speed * 0.825
+
+        elif FIRSTDITANCE * 1 / 128 >= dist - 0.8 * speed * DELTA_T > FIRSTDITANCE * 1 / 256:
+            return speed * 0.8
+
+        return speed * 0.775
     def _speed_upper_bound(self, p: np.ndarray):
         road = lambda x: p[0] * x ** 3 + p[1] * x ** 2 + p[2] * x + p[3]
         road_dx = lambda x: 3 * p[0] * x ** 2 + 2 * p[1] * x + p[2]
