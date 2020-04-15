4c6617fac7a336066e64273273567b72fa85702a
orenelmakis <oren.elmakis@campus.technion.ac.il>
2020-04-15 18:04:25 +0300

    Up_Date

---
+++ MPC/MPCMain.py
@@ -0,0 +1,13 @@
+###
+##Cost function
+
+##constraints
+## initial condition
+##kinmatic model
+##track bounds
+##saturation constraints
+
+
+##dynamic model
+##slip constrints
+
--- tests/system_runner_tests/ControlMainTest.py
+++ tests/system_runner_tests/ControlMainTest.py
@@ -1,6 +1,6 @@
 import signal
-
 from pyFormulaClientNoNvidia import messages
+
 # TODO: import path is probably going to change after integration into system runner
 from controller.controller import BasicController
 from system_runner.modules.ControlClient import ControlClient
@@ -10,7 +10,9 @@ from tests.system_runner_tests.utils import parse_file, print_file
 class Control:
     def __init__(self):
         # ControlClient reads messages from modules/state_est.messages and writes to modules/control.messages
-        self._client = ControlClient('messages/state.messages', 'messages/control.messages')
+        self._client = ControlClient(
+            "messages/state.messages", "messages/control.messages"
+        )
         self._running_id = 1
         self.message_timeout = 0.01
         self._controller = BasicController()
@@ -19,7 +21,9 @@ class Control:
 
     def start(self):
         self._client.connect(1)
-        self._client.set_read_delay(0.05)  # Sets the delay between reading new messages from sensors.messages
+        self._client.set_read_delay(
+            0.05
+        )  # Sets the delay between reading new messages from sensors.messages
         self._client.start()
 
     def stop(self):
@@ -32,7 +36,7 @@ class Control:
         formula_state_msg.Unpack(formula_state)
 
         if self.is_debug:
-            print(f'formula_state is: {formula_state}')
+            print(f"formula_state is: {formula_state}")
 
         driving_instructions = messages.control.DriveInstructions()
 
@@ -45,7 +49,7 @@ class Control:
         # driving_instructions.speed = out_msg.speed
 
         if self.is_debug:
-            print(f'driving_instructions is: {driving_instructions}')
+            print(f"driving_instructions is: {driving_instructions}")
 
         return driving_instructions
 
@@ -72,7 +76,9 @@ class Control:
                 pass
 
             try:
-                formula_state = self._client.get_formula_state_message(timeout=self.message_timeout)
+                formula_state = self._client.get_formula_state_message(
+                    timeout=self.message_timeout
+                )
                 driving_instruction = self.process_formula_state_message(formula_state)
                 self.send_message2server(formula_state.header.id, driving_instruction)
             except Exception as e:
@@ -102,11 +108,11 @@ def main():
 
     stop_all_threads()
     print("Finished Control")
-    print_file('messages/control.messages')
+    print_file("messages/control.messages")
     exit(0)
 
 
 if __name__ == "__main__":
-    for signame in ('SIGINT', 'SIGTERM'):
+    for signame in ("SIGINT", "SIGTERM"):
         signal.signal(getattr(signal, signame), shutdown)
     main()
